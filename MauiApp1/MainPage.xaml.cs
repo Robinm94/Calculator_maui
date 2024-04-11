3ab25b9390471a9b37a893a495ac66d5ad814bbb
robinm2812 <robinm2812@gmail.com>
2024-04-11 17:58:26 -0400

    Add project files.

---
+++ MauiApp1/MainPage.xaml.cs
@@ -0,0 +1,136 @@
+﻿namespace MauiApp1
+{
+    public partial class MainPage : ContentPage
+    {
+        private double firstNumber = 0;
+        private double secondNumber = 0;
+        private string currentOperation = "";
+
+        private bool isJustCalculated = false;
+        private bool isNumberJustEntered = false;
+
+
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+
+        void AddToDisplayNumber(object sender, EventArgs e)
+        {
+            Button button = (Button)sender;
+            string buttonText = button.Text;
+            // Append the button text to the Entry field
+            ResultEntry.Text += buttonText;
+        }
+
+        void Calculate(object sender, EventArgs e)
+        {
+            List<string> operations = new List<string>();
+            List<double> values = new List<double>();
+            isJustCalculated = true;
+            string calculationString = ResultEntry.Text;
+            if (calculationString == null || calculationString.Length <= 0 )
+            {
+                return;
+            }
+            
+
+        }
+
+            void OnButtonClicked(object sender, EventArgs e)
+        {
+            Button button = (Button)sender;
+            string buttonText = button.Text;
+
+            // Append the button text to the Entry field
+            ResultEntry.Text += buttonText;
+
+            switch (buttonText)
+            {
+                case "+":
+                case "-":
+                case "*":
+                case "/":
+                    currentOperation = buttonText;
+                    isNumberJustEntered = false;
+                    isJustCalculated = false;
+                    break;
+                case "=":
+                    firstNumber = PerformOperation(firstNumber, secondNumber, currentOperation);
+                    currentOperation = "";
+                    CalculateResult();
+                    // Clear the Entry field after calculating the result
+                    break;
+                case "C":
+                    firstNumber = 0;
+                    // Clear the Entry field when the "C" button is clicked
+                    ResultEntry.Text = "";
+                    break;
+                default:
+                    if (isJustCalculated)
+                    {
+                        ResultEntry.Text = "";
+                        isJustCalculated = false;
+                        ResultEntry.Text += buttonText;
+                        firstNumber = 0;
+                    }
+                    double number;
+                    if (double.TryParse(buttonText, out number))
+                    {
+                        if (currentOperation == "")
+                        {
+                            if (isNumberJustEntered)
+                            {
+                                firstNumber = firstNumber * 10 + number;
+                            }
+                            else
+                            {
+                                firstNumber = number;
+                            }
+                        }
+                        else
+                        {
+                            if (isNumberJustEntered)
+                            {
+                                secondNumber = secondNumber * 10 + number;
+                            }
+                            else
+                            {
+                                secondNumber = number;
+                            }
+                            
+                        }
+                    }
+                    isNumberJustEntered = true;
+                    break;
+            }
+        }
+
+        private double PerformOperation(double number1, double number2, string operation)
+        {
+            switch (operation)
+            {
+                case "+":
+                    return number1 + number2;
+                case "-":
+                    return number1 - number2;
+                case "*":
+                    return number1 * number2;
+                case "/":
+                    return number1 / number2;
+                default:
+                    return number2;
+            }
+        }
+
+        private void CalculateResult()
+        {
+
+            currentOperation = "";
+            isJustCalculated = true;
+            secondNumber = 0;
+            ResultEntry.Text = firstNumber.ToString();
+        }
+    }
+
+}
